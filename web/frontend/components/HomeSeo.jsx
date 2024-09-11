b450ea88e79853eb4e4ae31b3414172560c2409a
KaziMuktadirAhmed <kazi.muktadir@brainstation-23.com>
2024-09-11 17:48:43 +0600

    add extension and a industry meta description block

--- web/frontend/components/HomeSeo.jsx
+++ web/frontend/components/HomeSeo.jsx
@@ -1,5 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuthenticatedFetch, useShopQuery } from "../hooks";
+import {
+  AlphaCard,
+  Box,
+  Card,
+  Divider,
+  HorizontalStack,
+  Layout,
+  Page,
+  Select,
+  Text,
+  TextField,
+  VerticalStack,
+} from "@shopify/polaris";
+import IndustryInformation from "./HomepageSeoComponents/IndustryInformation";
 
 export default function HomeSeo() {
-  return <div>HomeSeo</div>;
+  const { data, error, status, isError, isLoading } = useShopQuery({
+    url: "/api/shop",
+  });
+  console.log(data);
+  return (
+    <>
+      {!isLoading ? (
+        <Page
+          fullWidth
+          compactTitle
+          divider
+          subtitle={data.domain}
+          title={<Text variant="headingLg">{data.name}</Text>}
+          primaryAction={{
+            content: "Save",
+            onAction: () => console.log("Save"),
+          }}
+        >
+          <Box paddingInlineStart={"32"} paddingInlineEnd={"32"}>
+            <IndustryInformation />
+            <Divider />
+          </Box>
+        </Page>
+      ) : (
+        <>Loading . . .</>
+      )}
+    </>
+  );
 }
---
+++ web/frontend/components/HomepageSeoComponents/IndustryInformation.jsx
@@ -0,0 +1,91 @@
+import React, { useCallback, useState } from "react";
+import {
+  Layout,
+  Box,
+  Text,
+  AlphaCard,
+  Select,
+  Icon,
+  VerticalStack,
+  TextField,
+} from "@shopify/polaris";
+import { EditMajor } from "@shopify/polaris-icons";
+
+export default function IndustryInformation() {
+  const [selected, setSelected] = useState("store");
+  const [otherIndustry, setOtherIndustry] = useState(null);
+  const handleSelectChange = useCallback((value) => setSelected(value), []);
+  const handleTextFieldChange = useCallback(
+    (value) => setOtherIndustry(value),
+    []
+  );
+
+  const options = [
+    {
+      label: "Store",
+      value: "store",
+    },
+    { label: "Arts and Crafts", value: "arts-and-crafts" },
+    { label: "Baby and Kids", value: "baby-and-kids" },
+    { label: "Books, Music and Video", value: "books-music-and-video" },
+    {
+      label: "Business equipment and Supplies",
+      value: "business-equipment-and-supplies",
+    },
+    { label: "Clothing", value: "clothing" },
+    { label: "Electronics", value: "electronics" },
+    { label: "Food and Drink", value: "food-and-drink" },
+    { label: "Hardware and Automotive", value: "hardware-and-automotive" },
+    { label: "Health and Beauty", value: "health-and-beauty" },
+    { label: "Home and Decor", value: "home-and-decor" },
+    { label: "Jewelry and Accessories", value: "jewelry-and-accessories" },
+    { label: "Outdoor and Garden", value: "outdoor-and-garden" },
+    { label: "Pet supplies", value: "pet-supplies" },
+    { label: "Restaurants", value: "restaurants" },
+    { label: "Services", value: "services" },
+    { label: "Sports and Recreation", value: "sports-and-recreation" },
+    { label: "Toys and Games", value: "toys-and-games" },
+    {
+      label: "Other",
+      value: "other",
+    },
+  ];
+  return (
+    <Box paddingBlockStart={"4"} paddingBlockEnd={"4"}>
+      <Layout>
+        <Layout.Section oneThird>
+          <Box paddingBlockEnd={"4"}>
+            <Text variant="headingMd">Industry</Text>
+          </Box>
+          <Box>
+            <Text variant="bodyMd">
+              Let search engines like Google know your industry
+            </Text>
+          </Box>
+        </Layout.Section>
+        <Layout.Section>
+          <Box>
+            <AlphaCard>
+              <VerticalStack gap={"6"}>
+                <Select
+                  helpText="Not sure what to choose? Use 'Store' as a default."
+                  label="Select industry"
+                  options={options}
+                  onChange={handleSelectChange}
+                  value={selected}
+                />
+                {selected == "other" && (
+                  <TextField
+                    value={otherIndustry}
+                    placeholder="Enter industry"
+                    onChange={handleTextFieldChange}
+                  ></TextField>
+                )}
+              </VerticalStack>
+            </AlphaCard>
+          </Box>
+        </Layout.Section>
+      </Layout>
+    </Box>
+  );
+}
