d8e051cd43f66671d562f5505a70d6a0ea23cf6c
shafi10 <shafiswe@gmail.com>
2024-10-08 15:28:48 +0600

    seo title and description

---
+++ web/frontend/components/AutoRedirect.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function AutoRedirect() {
+  return (
+    <div className="seo_error_list_container">
+      <div></div>
+      <div>AutoRedirect</div>
+    </div>
+  );
+}
